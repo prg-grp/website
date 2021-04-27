ff0c929ef09dd94afcf5fcb3c4c3d80dcf492e2f
Pascal Weisenburger <pascal.weisenburger@web.de>
2021-04-27 20:20:07 +0200

    navigation bar style and make sure it is visible without JS

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,6 @@
+---
+---
+
 $(document).ready(function() {
     $('a.abstract').click(function() {
         $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
@@ -6,4 +9,14 @@ $(document).ready(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     });
     $('.navbar-nav').find('a').removeClass('waves-effect waves-light');
+
+    {% if site.navbar_fixed %}
+    $('body').addClass('fixed-top-nav');
+    $('body > header').addClass('fixed-top');
+    {% else %}
+    $('body > header').addClass('sticky-top');
+    {% endif %}
+
+    $('.navbar').removeClass('navbar-expand').addClass('navbar-expand-md');
+    $('.navbar-collapse').addClass('text-right');
 });
