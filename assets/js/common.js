1a095a406927a594957a504aba6d1f06d479f8ea
Daniel Sokolowski <github@d.sokolowski.org>
2021-05-07 13:36:16 +0200

    Fix header jumpiness on load

--- assets/js/common.js
+++ assets/js/common.js
@@ -10,13 +10,5 @@ $(document).ready(function() {
     });
     $('.navbar-nav').find('a').removeClass('waves-effect waves-light');
 
-    {% if site.navbar_fixed %}
-    $('body').addClass('fixed-top-nav');
-    $('body > header').addClass('fixed-top');
-    {% else %}
-    $('body > header').addClass('sticky-top');
-    {% endif %}
-
     $('.navbar').removeClass('navbar-expand').addClass('navbar-expand-md');
-    $('.navbar-collapse').addClass('text-right');
 });
